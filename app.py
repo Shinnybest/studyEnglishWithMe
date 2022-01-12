1103206bb02802e850a71a1cfff0f469b49ec639
Lee <94841127+galaxian@users.noreply.github.com>
2022-01-12 09:16:31 +0900

    Update app.py
    로그인 후 접속 페이지 index.html -> main.html

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        return render_template('index.html')
+        return render_template('main.html')
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
