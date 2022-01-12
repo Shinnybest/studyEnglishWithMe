6db62c6c253cef8885ffe6a79c3c0714366804cb
Lee <94841127+galaxian@users.noreply.github.com>
2022-01-12 09:11:53 +0900

    Update app.py
    jinja2 템플릿 사용을 위한 변경

--- app.py
+++ app.py
@@ -165,9 +165,11 @@ def ewords():
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
+    words = list(db.words.find({}, {'_id': False}))
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        return render_template('index.html')
+        user_info = db.user.find_one({'id': payload['id']})
+        return render_template('index.html', words=words, user_info=user_info)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
